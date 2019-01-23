2dc988a28f2192b66dc1706f14fcc7b16cfb3ebc
That Lisa <thatlisajones@gmail.com>
2019-01-23 14:42:40 -0700

    Update naming

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,31 +1,31 @@
-// var friendsData = require("../data/friends");
-// //GET route for url /api/friends -- displays a JSON of all possible friends
+// var feelingsData = require("../data/feelings");
+// //GET route for url /api/feelings -- displays a JSON of all possible feelings
 
 // module.exports = function(app) {
-//     app.get("/api/friends", function(req, res) {
-//         return res.json(friendsData);
+//     app.get("/api/feelings", function(req, res) {
+//         return res.json(feelingsData);
 //     });
-//     //POST route /api/friends -- handles incoming survey results and compatibility logic
-//     app.post("/api/friends", function(req, res) {
-//         var newFriend = req.body;
+//     //POST route /api/feelings -- handles incoming survey results and compatibility logic
+//     app.post("/api/feelings", function(req, res) {
+//         var newfeeling = req.body;
 
-//         console.log(newFriend);
+//         console.log(newfeeling);
 
-//         //add code for the friend matching based on new friend details
-//         newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-//         console.log(`${newFriend.name} has been added to FakeFriend Finder.`);
-//         friends.push(newFriend);
-//         res.json(newFriend);
+//         //add code for the feeling matching based on new feeling details
+//         newfeeling.routeName = newfeeling.name.replace(/\s+/g, "").toLowerCase();
+//         console.log(`${newfeeling.name} has been added to Fakefeeling Finder.`);
+//         feelings.push(newfeeling);
+//         res.json(newfeeling);
 //     })};
-var friends = require("../data/friends");
+var feelings = require("../data/feelings");
 
 module.exports = function(app) {
-  // Return all friends found in friends.js as JSON
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
+  // Return all feelings found in feelings.js as JSON
+  app.get("/api/feelings", function(req, res) {
+    res.json(feelings);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/feelings", function(req, res) {
     console.log("req.body: ", req.body);
     console.log("body of post questions: ",req.body.questions);
 
@@ -37,30 +37,30 @@ module.exports = function(app) {
       user.questions[i] = parseInt(user.questions[i]);
     }
 
-    // default friend match is the first friend but result will be whoever has the minimum difference in questions
+    // default feeling match is the first feeling but result will be whoever has the minimum difference in questions
     var compatibilityValue = 0;
     var minimumDifference = 40;
 
-    // in this for-loop, start off with a zero difference and compare the user and the ith friend questions, one set at a time
+    // in this for-loop, start off with a zero difference and compare the user and the ith feeling questions, one set at a time
     //  whatever the difference is, add to the total difference
-    for(var i = 0; i < friends.length; i++) {
+    for(var i = 0; i < feelings.length; i++) {
       var totalDifference = 0;
-      for(var j = 0; j < friends[i].questions.length; j++) {
-        var difference = Math.abs(user.questions[j] - friends[i].questions[j]);
+      for(var j = 0; j < feelings[i].questions.length; j++) {
+        var difference = Math.abs(user.questions[j] - feelings[i].questions[j]);
         totalDifference += difference;
       }
 
-      // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+      // if there is a new minimum, change the best feeling index and set the new minimum for next iteration comparisons
       if(totalDifference < minimumDifference) {
         compatibilityValue = i;
         minimumDifference = totalDifference;
       }
     }
 
-    // after finding match, add user to friend array
-    friends.push(user);
+    // after finding match, add user to feeling array
+    feelings.push(user);
 
-    // send back to browser the best friend match
-    res.json(friends[compatibilityValue]);
+    // send back to browser the best feeling match
+    res.json(feelings[compatibilityValue]);
   });
 };
