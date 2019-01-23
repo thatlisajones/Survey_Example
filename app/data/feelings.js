f1a5411459d902460af6c80660b070df52a23001
That Lisa <thatlisajones@gmail.com>
2019-01-23 14:38:04 -0700

    Updated API

---
+++ app/data/feelings.js
@@ -0,0 +1,511 @@
+var feelings = [
+  {
+    
+    "feeling_group": "Joy",
+    "feeling_name":"Excited",
+    "feeling_emoji":"&#x1F601",
+    "feeling_color":"#ADFF2F",
+    "feeling_value":101
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Sensuous",
+    "feeling_emoji":"&#x1F60F",
+    "feeling_color":"#006400",
+    "feeling_value":102
+    
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Energetic",
+    "feeling_emoji":"&#x1F92A",
+    "feeling_color":"#228B22",
+    "feeling_value":103
+    
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Cheerful",
+    "feeling_emoji":"&#x1F31E",
+    "feeling_color":"#00FF00",
+    "feeling_value":104
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Creative",
+    "feeling_emoji":"&#x1F58C",
+    "feeling_color":"#32CD32",
+    "feeling_value":105
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Hopeful",
+    "feeling_emoji":"&#x1F91E",
+    "feeling_color":"#7CFC00",
+    "feeling_value":106
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Adventurous",
+    "feeling_emoji":"&#x1F343",
+    "feeling_color":"#6B8E23",
+    "feeling_value":107
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Fascinated",
+    "feeling_emoji":"&#x1F9D0",
+    "feeling_color":"#5DBF40",
+    "feeling_value":108
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Stimulated",
+    "feeling_emoji":"&#x1F61C",
+    "feeling_color":"#17FF0F",
+    "feeling_value":109
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Helpful",
+    "feeling_emoji":"&#x1F91D",
+    "feeling_color":"#98FB98",
+    "feeling_value":110
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Playful",
+    "feeling_emoji":"&#x1F33C",
+    "feeling_color":"#8AFFA5",
+    "feeling_value":111
+  },
+  {
+    "feeling_group": "Joy",
+    "feeling_name":"Delighted",
+    "feeling_emoji":"&#x1F917",
+    "feeling_color":"#00A300",
+    "feeling_value":112
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Calm",
+    "feeling_emoji":"&#x1F60C",
+    "feeling_color":"#4B0082",
+    "feeling_value":201
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Fulfilled",
+    "feeling_emoji":"&#x1F64C",
+    "feeling_color":"#8B008B",
+    "feeling_value":202
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Meditative",
+    "feeling_emoji":"&#x1F334",
+    "feeling_color":"#9932CC",
+    "feeling_value":203
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Harmonious",
+    "feeling_emoji":"&#x262E",
+    "feeling_color":"#9400D3",
+    "feeling_value":204
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Loving",
+    "feeling_emoji":"&#x1F49D",
+    "feeling_color":"#8A2BE2",
+    "feeling_value":205
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Trusting",
+    "feeling_emoji":"&#x1F932",
+    "feeling_color":"#9370DB",
+    "feeling_value":206
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Nurturing",
+    "feeling_emoji":"&#x1F431",
+    "feeling_color":"#BA55D3",
+    "feeling_value":207
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Kind",
+    "feeling_emoji":"&#x1F46D",
+    "feeling_color":"#FF00FF",
+    "feeling_value":208
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Validated",
+    "feeling_emoji":"&#x2705",
+    "feeling_color":"#DA70D6",
+    "feeling_value":209
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Generous",
+    "feeling_emoji":"&#x1F49C",
+    "feeling_color":"#EE82EE",
+    "feeling_value":210
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Secure",
+    "feeling_emoji":"&#x1F510",
+    "feeling_color":"#DDA0DD",
+    "feeling_value":211
+  },
+  {
+    "feeling_group": "Peace",
+    "feeling_name":"Thankful",
+    "feeling_emoji":"&#x1F64F",
+    "feeling_color":"#E6E6FA",
+    "feeling_value":212
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Loyal",
+    "feeling_emoji":"&#x1F415",
+    "feeling_color":"#F0F8FF",
+    "feeling_value":301
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Needed",
+    "feeling_emoji":"&#x1F436",
+    "feeling_color":"#B0E0E6",
+    "feeling_value":302
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Appreciated",
+    "feeling_emoji":"&#x1F48E",
+    "feeling_color":"#87CEFA",
+    "feeling_value":303
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Respected",
+    "feeling_emoji":"&#x1F3C6",
+    "feeling_color":"#87CEEB",
+    "feeling_value":304
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Dignified",
+    "feeling_emoji":"&#x1F451",
+    "feeling_color":"#00BFFF",
+    "feeling_value":305
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Blessed",
+    "feeling_emoji":"&#x1F381",
+    "feeling_color":"#1E90FF",
+    "feeling_value":306
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Lucky",
+    "feeling_emoji":"&#x1F340",
+    "feeling_color":"#6495ED",
+    "feeling_value":307
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Discerning",
+    "feeling_emoji":"&#x1F9D0",
+    "feeling_color":"#4682B4",
+    "feeling_value":308
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Loved",
+    "feeling_emoji":"&#x1F60D",
+    "feeling_color":"#7B68EE",
+    "feeling_value":309
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Admirable",
+    "feeling_emoji":"&#x1F607",
+    "feeling_color":"#6A5ACD",
+    "feeling_value":310
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Successful",
+    "feeling_emoji":"&#x1F947",
+    "feeling_color":"#4169E1",
+    "feeling_value":311
+  },
+  {
+    "feeling_group": "Confidence",
+    "feeling_name":"Deserving",
+    "feeling_emoji":"&#x1F4AF",
+    "feeling_color":"#0000CD",
+    "feeling_value":312
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Sorry",
+    "feeling_emoji":"&#x1F630",
+    "feeling_color":"#FF4500",
+    "feeling_value":401
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Stupid",
+    "feeling_emoji":"&#x1F92F",
+    "feeling_color":"#F7A036",
+    "feeling_value":402
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Inferior",
+    "feeling_emoji":"&#x1F949",
+    "feeling_color":"#F2AC92",
+    "feeling_value":403
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Isolated",
+    "feeling_emoji":"&#x1F614",
+    "feeling_color":"#DAB68B",
+    "feeling_value":404
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Uninterested",
+    "feeling_emoji":"&#x1F4A4",
+    "feeling_color":"#E6830A",
+    "feeling_value":405
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Disappointed",
+    "feeling_emoji":"&#x1F61E",
+    "feeling_color":"#FFD8A8",
+    "feeling_value":406
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Guilty",
+    "feeling_emoji":"&#x2623",
+    "feeling_color":"#FF8C00",
+    "feeling_value":407
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Ashamed",
+    "feeling_emoji":"&#x1F613",
+    "feeling_color":"#FF6347",
+    "feeling_value":408
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Depressed",
+    "feeling_emoji":"&#x1F62D",
+    "feeling_color":"#FFD4CC",
+    "feeling_value":409
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Lonely",
+    "feeling_emoji":"&#x1F940",
+    "feeling_color":"#F4AC9F",
+    "feeling_value":410
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Bored",
+    "feeling_emoji":"&#x1F417",
+    "feeling_color":"#FF7F50",
+    "feeling_value":411
+  },
+  {
+    "feeling_group": "Sadness",
+    "feeling_name":"Tired",
+    "feeling_emoji":"&#x1F634",
+    "feeling_color":"#FFCEBD",
+    "feeling_value":412
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Critical",
+    "feeling_emoji":"&#x1F50D",
+    "feeling_color":"#FF6347",
+    "feeling_value":501
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Spiteful",
+    "feeling_emoji":"&#x1F612",
+    "feeling_color":"#FF4500",
+    "feeling_value":502
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Outraged",
+    "feeling_emoji":"&#x1F92C",
+    "feeling_color":"#8B0000",
+    "feeling_value":503
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Bitter",
+    "feeling_emoji":"&#x2639",
+    "feeling_color":"#B22222",
+    "feeling_value":504
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Resentful",
+    "feeling_emoji":"&#x1F624",
+    "feeling_color":"#DC143C",
+    "feeling_value":505
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Hurt",
+    "feeling_emoji":"&#x1F622",
+    "feeling_color":"#CD5C5C",
+    "feeling_value":506
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Skeptical",
+    "feeling_emoji":"&#x1F928",
+    "feeling_color":"#F08080",
+    "feeling_value":507
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Irritated",
+    "feeling_emoji":"&#x1F616",
+    "feeling_color":"#E9967A",
+    "feeling_value":508
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Frustrated",
+    "feeling_emoji":"&#x1F623",
+    "feeling_color":"#990000",
+    "feeling_value":509
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Dismissive",
+    "feeling_emoji":"&#x1F644",
+    "feeling_color":"#FFADAD",
+    "feeling_value":510
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Distant",
+    "feeling_emoji":"&#x1F3D4",
+    "feeling_color":"#FA8072",
+    "feeling_value":511
+  },
+  {
+    "feeling_group": "Anger",
+    "feeling_name":"Jealous",
+    "feeling_emoji":"&#x1F620",
+    "feeling_color":"#8B0000",
+    "feeling_value":512
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Confused",
+    "feeling_emoji":"&#x1F615",
+    "feeling_color":"#FFFFCC",
+    "feeling_value":601
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Rejected",
+    "feeling_emoji":"&#x274C",
+    "feeling_color":"#FFFF99",
+    "feeling_value":602
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Helpless",
+    "feeling_emoji":"&#x1F937",
+    "feeling_color":"#FFFF66",
+    "feeling_value":603
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Belittled",
+    "feeling_emoji":"&#x1F632",
+    "feeling_color":"#FFFF33",
+    "feeling_value":604
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Insecure",
+    "feeling_emoji":"&#x1F633",
+    "feeling_color":"#FFFF00",
+    "feeling_value":605
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Anxious",
+    "feeling_emoji":"&#x1F630",
+    "feeling_color":"#FFFFE0",
+    "feeling_value":606
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Overwhelmed",
+    "feeling_emoji":"&#x1F30B",
+    "feeling_color":"#FFFACD",
+    "feeling_value":607
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Embarrassed",
+    "feeling_emoji":"&#x1F92D",
+    "feeling_color":"#EEE8AA",
+    "feeling_value":608
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Inadequate",
+    "feeling_emoji":"&#x2B55",
+    "feeling_color":"#FAFAD2",
+    "feeling_value":609
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Insignificant",
+    "feeling_emoji":"&#x1F400",
+    "feeling_color":"#CCCC00",
+    "feeling_value":610
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Discouraged",
+    "feeling_emoji":"&#x1F61F",
+    "feeling_color":"#F0F093",
+    "feeling_value":611
+  },
+  {
+    "feeling_group": "Fear",
+    "feeling_name":"Frightened",
+    "feeling_emoji":"&#x1F631",
+    "feeling_color":"#FFFF85",
+    "feeling_value":612
+  }
+  ]
+  
+  module.exports = feelings;
